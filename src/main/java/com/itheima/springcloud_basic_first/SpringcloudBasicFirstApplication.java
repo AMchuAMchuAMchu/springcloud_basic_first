6fafb8eb6281953ddff89212c8b027c10e1f020a
AMchuAMchuAMchu <2466130840@qq.com>
2022-10-05 17:13:11 +0800

    AM 01st commit

---
+++ src/main/java/com/itheima/springcloud_basic_first/SpringcloudBasicFirstApplication.java
@@ -0,0 +1,13 @@
+package com.itheima.springcloud_basic_first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudBasicFirstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudBasicFirstApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itheima/springcloud_basic_first/SpringcloudBasicFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itheima.springcloud_basic_first;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudBasicFirstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
